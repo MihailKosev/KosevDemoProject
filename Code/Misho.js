69d206db2ba696ca28c8a4547346595ee0cf8e2a
MihailKosev <123077729+MihailKosev@users.noreply.github.com>
2023-01-20 15:03:23 +0200

    Update Misho.js

--- Code/Misho.js
+++ Code/Misho.js
@@ -1,9 +1,6 @@
 function demo(){
 console.log("Hello");
   console.log("Hi");
+  console.log("Couldn't understand what happaned");
 }
-<<<<<<< HEAD:Code/demo.js
-demo()demo();
-=======
-demo()
->>>>>>> eca45abbf3c5f8f5a8938b2b838aedd266e47a01:Code/Misho.js
+demo();
