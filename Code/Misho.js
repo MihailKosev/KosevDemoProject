098335caea4ef5fa90f98f30b146b18bc29f158c
MihailKosev <nemrunkai_deistvai@abv.bg>
2023-01-20 15:00:53 +0200

    Create Misho.js

---
+++ Code/Misho.js
@@ -0,0 +1,9 @@
+function demo(){
+console.log("Hello");
+  console.log("Hi");
+}
+<<<<<<< HEAD:Code/demo.js
+demo()demo();
+=======
+demo()
+>>>>>>> eca45abbf3c5f8f5a8938b2b838aedd266e47a01:Code/Misho.js
